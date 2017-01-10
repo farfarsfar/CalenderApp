3e8cc930d99559def6d63fff2f0661cc0f2bb765
farfarsfar <fabianrislundblad@gmail.com>
2017-01-10 10:52:47 +0100

    new test

--- frontend/app/event-list/event-list.test.js
+++ frontend/app/event-list/event-list.test.js
@@ -1,6 +1,13 @@
 import chai, { expect } from 'chai';
 import eventList from './event-list.component';
 
+const exampleEvents = [
+  {
+    date: '23 aug 2017',
+    time: '21:44',
+    description: 'See Charles'
+  }
+];
 
 describe('event list', () => {
   var element;
@@ -10,22 +17,21 @@ describe('event list', () => {
   beforeEach(() => {
     angular.mock.module('eventList');
 
-    inject(function($rootScope, $compile){
+    inject(function($rootScope, $compile, $componentController){
     scope = $rootScope.$new();
     element = angular.element('<event-list></event-list>');
     element = $compile(element)(scope);
     scope.$apply();
+    controller = $componentController('eventList', {$scope: scope});
     });
 
-    controller = element.controller('eventList');
   });
 
   it('should be defined', () => {
     expect(controller).to.not.be.undefined;
   });
   it('should have an example event set', () => {
-    // TODO: how to access scope?
-    //expect(scope.events).to.deep.equal(exampleEvents);
+    expect(scope.events).to.deep.equal(exampleEvents);
   });
   it('should render the event text', () => {
     const p = element.find('p');
