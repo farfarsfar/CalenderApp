cab96aec63adc40900826e56f461377a110189ea
farfarsfar <fabianrislundblad@gmail.com>
2017-01-16 13:34:07 +0100

    [frontend] starting on some api requests

--- frontend/app/event-list/event-list.controller.js
+++ frontend/app/event-list/event-list.controller.js
@@ -1,4 +1,4 @@
-export default function() {
+export default function($http) {
   this.$onInit = console.log('event component initialized');
   this.events = [
     {
@@ -12,6 +12,37 @@ export default function() {
                        time: new Date().toLocaleTimeString(),
                        description: this.addMe };
     this.events.push(newEvent);
+    const toPost =  { "Title": "Event 432",
+                      "Start_Time": "2017-01-11T10:44:12.030105Z",
+                      "End_Time": "2017-01-11T11:01:04.537832Z"
+                    };
+    this.postEvent(toPost);
+    this.fetchEvents();
+
     this.addMe = '';
+  }
+  this.sayHiOnChange = (msg) => {
+    console.log(`input field says ${msg}`)
+  }
+  this.fetchEvents = () => {
+    $http.get('/api/').then((resp) => {
+      console.log("success: ", resp);
+    }, (err) => { 
+      console.log("error: ", err)
+    }
+    )
+  }
+  this.postEvent = (event) => {
+    $http({
+      method: 'POST',
+      url: '/api/',
+      data: event
+    }).then(function successCallback(response) {
+    // this callback will be called asynchronously
+    // when the response is available
+    }, function errorCallback(response) {
+    // called asynchronously if an error occurs
+    // or server returns response with an error status.
+  });
   }
 }
