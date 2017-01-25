cd8d6bf2d029c507512c5677f488ed3656df3275
rattana-p <r.pongpaew@gmail.com>
2017-01-25 23:48:39 +0100

    style for today and event

--- frontend/app/event-list/event-list.controller.js
+++ frontend/app/event-list/event-list.controller.js
@@ -3,6 +3,8 @@ import $ from 'jquery';
 export default function ($scope, $rootScope, $http) {
 	$rootScope.x = 0;
 	$rootScope.z = 0;
+	
+	
 	// function to show current month 
 	$scope.initCal = function () {
 			$rootScope.time_now = moment().format('YYYY, MMMM');
@@ -13,7 +15,7 @@ export default function ($scope, $rootScope, $http) {
 			$rootScope.x = 0;
 			$rootScope.z = 0;
 			$scope.showday();
-			$scope.showevent();
+			
 		}
 		// function to show date in HTML 
 	$scope.showday = function () {
@@ -29,14 +31,23 @@ export default function ($scope, $rootScope, $http) {
 	};
 	// show calendar body 
 	$scope.insert_day = function () {
-			var board = document.getElementById('show_totaldays');
+	var board = document.getElementById('show_totaldays');
+	$http.get("http://localhost:3000/api/").then(function (response) {
+		//	$scope.myWelcome = [];
+		$scope.myWelcome = response.data;
+		
+		dogme: for (var d = 0; d < $scope.myWelcome.length; d++) {
+			
+			
+			$rootScope.event_date = moment($scope.myWelcome[d].Start_Time).format('YYYY, MM, DD');
+			//	console.log( $rootScope.event_date );
 			for (var j = 0; j < $rootScope.start_day; j++) {
 				var backside = document.createElement("div");
 				backside.innerHTML = ' ';
 				backside.className = 'unday day';
 				board.appendChild(backside);
 			}
-			for (var i = 1; i <= $rootScope.showdays; i++) {
+			loveme: for (var i = 1; i <= $rootScope.showdays; i++) {
 				var backside = document.createElement("div");
 				backside.innerHTML = '' + [i] + '';
 				backside.className = 'day';
@@ -44,13 +55,28 @@ export default function ($scope, $rootScope, $http) {
 				board.appendChild(backside);
 				var ooo = document.getElementById($rootScope.iddate + [i]).id;
 				var ttt = moment().format('YYYY, MM, DD');
-				var xxx = document.getElementById(ooo).id;
-				if (ttt === ooo) {
+				$rootScope.xxx = document.getElementById(ooo).id;
+				if (ttt === ooo && d == 0 ) {
 					ooo = document.getElementById(ooo);
 					ooo.className += "today";
+					
 				}
-			}
-		}
+				/*console.log($rootScope.iddate + [i]);
+				console.log($rootScope.event_date); */
+				if (($rootScope.iddate + [i]) == $rootScope.event_date) {
+					$scope.event_start = moment($scope.myWelcome[d].Start_Time).format('kk:mm');
+					$scope.event_stop = moment($scope.myWelcome[d].End_Time).format('kk:mm');
+					$scope.event_title = $scope.myWelcome[d].Title;
+					console.log('its same');
+					var myEl = angular.element(document.getElementById($rootScope.event_date));
+					myEl.append('<div class="eventdiv"> <span class="tid_start">'+ $scope.event_start +'</span> - <span class="tid_start">'+ $scope.event_stop +'</span><br><span class="event_title">'+ $scope.event_title +'</span> </div>');
+					
+				}
+				
+			} 
+		} 
+	});
+}
 		// function to show last month
 	$scope.last_month = function () {
 		$('.day').remove();
@@ -62,13 +88,16 @@ export default function ($scope, $rootScope, $http) {
 		$rootScope.iddate = moment($rootScope.totalday_from_last_month).format('YYYY, MM, ');
 		$rootScope.month_header.innerHTML = $rootScope.time_from_last_month;
 		$rootScope.start_day = moment($rootScope.totalday_from_last_month).format('d');
+		
 		// show total days in month
 		$scope.showdays_lastmonth = moment($rootScope.totalday_from_last_month).daysInMonth();
 		document.getElementById('show_howmany_day').innerHTML = $scope.showdays_lastmonth + " days";
 		// go to function to show calendar body
 		$scope.insert_day($rootScope.showdays = $scope.showdays_lastmonth, $rootScope.iddate);
 		$rootScope.z = 0;
+		
 	};
+	
 	// function to show next month  
 	$scope.next_month = function () {
 		$('.day').remove();
@@ -79,20 +108,17 @@ export default function ($scope, $rootScope, $http) {
 		$rootScope.totalday_from_next_month = moment($rootScope.time_from_last_month).add($rootScope.z, 'M').format('YYYY, MM, DD');
 		$rootScope.iddate = moment($rootScope.totalday_from_next_month).format('YYYY, MM, ');
 		$rootScope.month_header.innerHTML = $rootScope.time_from_next_month;
-		$rootScope.start_day = moment($scope.totalday_from_next_month).format('d');
+		$rootScope.start_day = moment($rootScope.totalday_from_next_month).format('d');
+		
 		// show total days in month
-		$scope.showdays_nextmonth = moment($scope.totalday_from_next_month).daysInMonth();
+		$scope.showdays_nextmonth = moment($rootScope.totalday_from_next_month).daysInMonth();
 		document.getElementById('show_howmany_day').innerHTML = $scope.showdays_nextmonth + " days";
 		// go to function to show calendar body
 		$scope.insert_day($rootScope.showdays = $scope.showdays_nextmonth, $rootScope.iddate);
 		$rootScope.x = 0;
 	};
 	
-	$scope.showevent = function () {
-		var myEl = angular.element(document.querySelector('.daytoday'));
-		myEl.append('<div class="eventdiv"> <span class="tidstart">13:00 </span> <span class="eventtital"> group study </span>  </div> <div class="eventdiv"> <span class="tidstart">15:00</span> <span class="eventtital"> cooking </span> </div> <div class="eventdiv"> <span class="tidstart">20:00</span> <span class="eventtital"> watching tv </span>  </div>');
-	}
-  $scope.showAdmin = () => {
+$scope.showAdmin = () => {
     $scope.$broadcast('showHideAdmin', true);
   }
 }
